1c03bb76ec95167cbf2953055faf5e467f831f1c
hhhhfloat <hhhhulewen@163.com>
2026-04-06 17:47:05 +0800

    Add more codes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,10 +2,8 @@ package org.example;
 
 import java.util.Scanner;
 import org.example.Objects.SomeClass;
-/**
- * Hello world!
- *
- */
+import org.example.Functions.SomeFunctions;
+
 public class App 
 {
     public static void main( String[] args )
@@ -14,5 +12,9 @@ public class App
         System.out.println(sc.nextInt());
         SomeClass so = new SomeClass();
         System.out.println(so.GetName());
+        SomeFunctions sf = new SomeFunctions();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(sf.aaa());
+        }
     }
 }
---
+++ src/main/java/org/example/Functions/SomeFunctions.java
@@ -0,0 +1,8 @@
+package org.example.Functions;
+
+public class SomeFunctions {
+    public int aaa()
+    {
+        return 100;
+    }
+}
