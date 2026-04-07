37f8844fb9c934502f4d8ee20441646677bb461a
hhhhfloat <hhhhulewen@163.com>
2026-04-07 14:09:30 +0800

    Add more codes

--- src/main/java/org/example/Functions/Logics.java
+++ src/main/java/org/example/Functions/Logics.java
@@ -7,8 +7,13 @@ public class Logics {
     public ArrayDeque<int[]> Linky(int MAPX, int MAPY, String map_, int[] p1, int[] p2)
     {
         ArrayDeque<int[]> path = null;
+        int p1x = p1[0], p1y = p1[1], p2x = p2[0], p2y = p2[1];
 
-
+        // 共线检测
+        if(p1x == p2x)
+        {
+            int one = (p1y - p2y > 0)? 1 : -1;
+        }
 
         return path;
     }
