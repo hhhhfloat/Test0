32711d53f036191ac4195541d093cc4abaf4e1ff
hhhhfloat <hhhhulewen@163.com>
2026-04-07 14:06:20 +0800

    Add more codes

---
+++ src/main/java/org/example/Functions/Logics.java
@@ -0,0 +1,15 @@
+package org.example.Functions;
+
+import java.util.ArrayDeque;
+
+
+public class Logics {
+    public ArrayDeque<int[]> Linky(int MAPX, int MAPY, String map_, int[] p1, int[] p2)
+    {
+        ArrayDeque<int[]> path = null;
+
+
+
+        return path;
+    }
+}
