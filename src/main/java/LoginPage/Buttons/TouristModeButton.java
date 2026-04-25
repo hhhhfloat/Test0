7db8c077647ab931235957d589aca247a50af32f
VagrantX <3519344040@qq.com>
2026-04-25 23:54:27 +0800

    登陆界面Ver 0.4 增加了一些改动

--- src/main/java/LoginPage/Buttons/TouristModeButton.java
+++ src/main/java/LoginPage/Buttons/TouristModeButton.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 public class TouristModeButton {
     public static Button touristMode() {
         Button tourist = new Button("Tourist Mode");
-        tourist.setPrefSize(50, 400);
+        tourist.setPrefSize(400, 50);
         tourist.setOnAction(event -> {
 
         });
