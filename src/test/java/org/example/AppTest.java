16652f8ae6708ade9581ea118d1377fc8e8ee323
hhhhfloat <hhhhulewen@163.com>
2026-04-25 17:43:15 +0800

    Added pickPath(Debuggee)

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,10 +4,10 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.ArrayDeque;
+import java.util.regex.Pattern;
 
 import org.example.Functions.*;
 
@@ -46,81 +46,15 @@ public class AppTest
         assertTrue(true);
     }
 
-    // Test the Logics.Linky
     // Test Functions have to be named after "test", like "testABC"
-    public void _testLinkyLogic() throws InterruptedException {
-        int MAPX = 10, MAPY = 10;
-        String map = "";
-        map += " 2 642 8  ";
-        map += "   9   6 2";
-        map += " 2     2  ";
-        map += "  8 7  0  ";
-        map += " 5     57 ";
-        map += " 20    1  ";
-        map += "0514  91  ";
-        map += "  5       ";
-        map += "   2    0 ";
-        map += "        2 ";
-        int[][] Map = new int[MAPX][MAPY];
-        for (int i = 0; i < MAPX; i++) {
-            for (int j = 0; j < MAPY; j++) {
-                char c = map.charAt(i * MAPY + j);
-                Map[i][j] = (c == ' ') ? -1 : (c - '0');
-            }
-        }
 
-        int[][][] TestInput = {
-                // Straight Test
-                {{2, 7}, {2, 1}},
-                {{0, 1}, {2, 1}},
-                // Row Detect Test
-                {{3, 7}, {5, 2}},// Two twists
-                {{8, 3}, {9, 8}},// One twist
-                // Column Detect Test
-                {{4, 8}, {3, 4}}, // Two twists (Only this case)
-                // Special
-                {{6, 1}, {7, 2}},
-                {{5, 7}, {6, 7}}
-        };
-        Logics logics = new Logics();
-        int count = 0;
-        int TestNumber = TestInput.length;
-        while (count < TestNumber) // Run for TestNumber Times
-        {
-            Clear();
-            PrintMap(Map, MAPX, MAPY);
-            Sleep(1000);
-
-            ArrayDeque<int[]> path = logics.Linky(
-                    MAPX, MAPY, Map,
-                    TestInput[count][0], TestInput[count][1]
-            );
-            if (!path.isEmpty() && path.peek()[0] == -1) {
-                System.out.println("   INVALID INPUT   ");
-            } else if (path.isEmpty()) {
-                System.out.println("   IMPOSSIBLE CONNECTION   ");
-            } else {
-                int[][] mapWithPath = ShowPath(path, Map, MAPX, MAPY);
-                Clear();
-                Map[TestInput[count][0][0]][TestInput[count][0][1]] = -1;
-                Map[TestInput[count][1][0]][TestInput[count][1][1]] = -1;
-                PrintMap(mapWithPath, MAPX, MAPY);
-                PrintPath(path);
-
-            }
-            Sleep(3000);
-            count++;
-        }
-
-    }
-
-    public int[][] ShowPath(ArrayDeque<int[]> path, int[][] map_, int MAPX, int MAPY) {
+    public int[][] ShowPath(HashSet<Point> path, int[][] map_, int MAPX, int MAPY) {
         int[][] temp = new int[MAPX][MAPY];
         for (int i = 0; i < MAPX; i++) {
             System.arraycopy(map_[i], 0, temp[i], 0, MAPY);
         }
-        for (int[] p : path) {
-            temp[p[0]][p[1]] = '#';
+        for (Point p : path) {
+            temp[p.x()][p.y()] = '#';
         }
         return temp;
     }
@@ -151,65 +85,11 @@ public class AppTest
     }
 
     // Use this to Print the path
-    public void PrintPath(ArrayDeque<int[]> path) {
-        for (int[] p : path) {
-            System.out.printf("(%d, %d) ", p[0], p[1]);
+    public void PrintPath(HashSet<Point> path) {
+        for (Point p : path) {
+            System.out.printf("(%d, %d) ", p.x(), p.y());
         }
         System.out.println();
-    }
-
-
-    /*
-     * 自动解题
-     * */
-    public void _testAutoSolve() throws InterruptedException {
-        int MAPX = 6, MAPY = 6;
-        String map = "";
-        map += "54321 ";
-        map += "98765 ";
-        map += "54321 ";
-        map += "056787";
-        map += "129270";
-        map += "5   51";
-//
-        int[][] Map = new int[MAPX][MAPY];
-        for (int i = 0; i < MAPX; i++) {
-            for (int j = 0; j < MAPY; j++) {
-                char c = map.charAt(i * MAPY + j);
-                Map[i][j] = (c == ' ') ? -1 : (c - '0');
-            }
-        }
-
-        Logics logics = new Logics();
-
-        while (!isComplete(Map)) {
-            Clear();
-            //PrintMap(Map, MAPX, MAPY);
-            //Sleep(1000);
-
-            // 自动找到一个路径
-            ArrayDeque<int[]> path = logics.HintSolution(MAPX, MAPY, Map);
-
-            //Sleep(2000);
-            if (path.isEmpty()) {
-                System.out.println("没有路径！");
-                break;
-            } else {
-                int[] p1 = path.peek();
-                int[] p2 = path.getLast();
-                int[][] mapWithPath = ShowPath(path, Map, MAPX, MAPY);
-                //Clear();
-
-                Map[p1[0]][p1[1]] = -1;
-                Map[p2[0]][p2[1]] = -1;
-                PrintMap(mapWithPath, MAPX, MAPY);
-                PrintPath(path);
-
-            }
-
-            Sleep(1000);
-        }
-
     }
 
     // 检测完成的方法
@@ -229,5 +109,43 @@ public class AppTest
         System.out.printf("(%d, %d) \n", p[0], p[1]);
     }
 
+    // 测试pickPath
+    public void _testpickPath() throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(Pattern.compile(","));
+        int MAPX = 6, MAPY = 6;
+        LinkyMap level = new LinkyMap(MAPX, MAPY);
+
+        // 测试数据
+        Point[][] p = {
+                {new Point(5,0),new Point(5,4)},
+                {new Point(0,4),new Point(2,4)}
+        };
+        int l = p.length;
+        int count = 0;
+
+        while (!isComplete(level.getMap()) && count < l) {
+            PrintMap(level.getMap(),MAPX,MAPY);
+
+            System.out.println();
+            System.out.println();
+
+            Point p1 = p[count][0];
+            Point p2 = p[count][1];
+            HashSet<Point>path = level.pickPath(p1,p2);
+            int[][] Map = ShowPath(path,level.getMap(),MAPX,MAPY);
+            PrintMap(Map, MAPX,MAPY);
+            HashSet<Point> del = new HashSet<>();
+            del.add(p1);del.add(p2);
+            level.delNumMap(del);
+            count++;
+
+            Sleep(2000);
+            System.out.println();
+            System.out.println();
+            System.out.println();
+        }
+        PrintMap(level.getMap(),MAPX,MAPY);
+    }
 
 }
