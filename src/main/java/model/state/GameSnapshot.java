35e64181f5187f50e3c7845dbed58d322035c1a9
hhhhfloat <hhhhulewen@163.com>
2026-04-27 21:07:19 +0800

    Refactoring

--- src/main/java/model/state/GameSnapshot.java
+++ src/main/java/model/state/GameSnapshot.java
@@ -1,6 +1,15 @@
 package model.state;
 
+import java.time.LocalDateTime;
+
 // 自定义快照——棋盘状态、分数、时间
 public class GameSnapshot {
+    private int[][] girdData;
+    private int currentScore;
+    private int remainTime;
+    private boolean isPaused;
+    private int levelNum;
+    private LocalDateTime saveTime;
+
 
 }
