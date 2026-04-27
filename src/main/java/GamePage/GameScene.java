2f52dd033b5bfc92c6767308697019a28ab2295c
VagrantX <3519344040@qq.com>
2026-04-27 11:08:02 +0800

    App Ver 0.6 准备完善棋盘功能

--- src/main/java/GamePage/GameScene.java
+++ src/main/java/GamePage/GameScene.java
@@ -6,7 +6,6 @@ import javafx.scene.layout.VBox;
 
 public class GameScene {
     public static Scene getGameScene() {
-        int[][] board = {{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
         VBox gameboard = new VBox(Grid.grid(10, 10, 50, 50));
         int points = 0;
         ScoreLabel scoreLabel = new ScoreLabel(points);
--- src/main/java/GamePage/Test.java
+++ src/main/java/GamePage/Test.java
@@ -6,27 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.Functions.LinkyMap;
 
-public class Test extends Application {
-    @Override
-    public void start(Stage stage) {
-        // 情况1：不设置 maxWidth
-        Button button1 = new Button("按钮1 - 默认");
-        button1.setStyle("-fx-background-color: lightcoral;");
+public class Test  {
 
-        // 情况2：设置 maxWidth 为无限
-        Button button2 = new Button("按钮2 - maxWidth=无限");
-        button2.setPrefWidth(30);
-        button2.setMaxWidth(Double.MAX_VALUE);
 
-        button2.setStyle("-fx-background-color: lightgreen;");
-
-        VBox vbox = new VBox(10, button1, button2);
-        vbox.setPrefWidth(300);
-        vbox.setStyle("-fx-border-color: black; -fx-padding: 10;");
-
-        stage.setScene(new Scene(vbox));
-        stage.setTitle("maxWidth 对比");
-        stage.show();
-    }
 }
