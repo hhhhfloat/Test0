a9f847551c94a9254a2e98235e33b02d81c15e24
hhhhfloat <hhhhulewen@163.com>
2026-04-27 16:24:43 +0800

    Added auto sRandMap

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -116,7 +116,7 @@ public class AppTest
         int Count = 0;
         int MAPX = 10, MAPY = 10;
         int nType = 50;
-        LinkyMap level = new LinkyMap(12, 12, 0);
-        //PrintMap(level.getMap());
+        LinkyMap level = new LinkyMap(12, 12, 1);
+        PrintMap(level.getMap());
     }
 }
