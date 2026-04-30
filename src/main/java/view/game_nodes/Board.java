40a127f1082f7a483b9b3eea271bacf2dea4b37e
hhhhfloat <hhhhulewen@163.com>
2026-04-30 20:22:32 +0800

    Added Pair Mode Checking and Map Generating logic

--- src/main/java/view/game_nodes/Board.java
+++ src/main/java/view/game_nodes/Board.java
@@ -3,7 +3,7 @@ package view.game_nodes;
 import model.entity.LinkyMap;
 
 public class Board {
-    LinkyMap linkyMap = new LinkyMap(10,10,10);
+    LinkyMap linkyMap = new LinkyMap(10,10,1,true);
     int[][] board = linkyMap.getMap();
 
     public Board() throws InterruptedException {
