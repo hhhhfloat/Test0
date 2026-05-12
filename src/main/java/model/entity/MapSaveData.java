2698b0ed1d12637987cda8db33ab79ec23be2773
hhhhfloat <hhhhulewen@163.com>
2026-05-12 15:43:42 +0800

    初步更新存档,增加了对随机图片组的适配

--- src/main/java/model/entity/MapSaveData.java
+++ src/main/java/model/entity/MapSaveData.java
@@ -3,15 +3,18 @@ package model.entity;
 public class MapSaveData {
     private int[][] easyMap;
     private int[][] hardMap;
-    private int isPair;
+    private boolean isPair;
+    private int easyScore, hardScore;
 
     public MapSaveData(){}
 
-    public MapSaveData(int[][] easyMap, int[][] hardMap, boolean isPair)
+    public MapSaveData(int[][] easyMap, int[][] hardMap, boolean isPair, int easyScore, int hardScore)
     {
         this.easyMap = easyMap;
         this.hardMap = hardMap;
-        this.isPair = isPair?2:1;
+        this.isPair = isPair;
+        this.easyScore = easyScore;
+        this.hardScore = hardScore;
     }
     public int[][] getEasyMap(){return easyMap;}
     public void setEasyMap(int[][] easyMap) {
@@ -22,5 +25,29 @@ public class MapSaveData {
     }
     public void setHardMap(int[][] hardMap) {
         this.hardMap = hardMap;
+    }
+
+    public int getEasyScore() {
+        return easyScore;
+    }
+
+    public int getHardScore() {
+        return hardScore;
+    }
+
+    public boolean getIsPair() {
+        return isPair;
+    }
+
+    public void setEasyScore(int easyScore) {
+        this.easyScore = easyScore;
+    }
+
+    public void setHardScore(int hardScore) {
+        this.hardScore = hardScore;
+    }
+
+    public void setIsPair(boolean isPair) {
+        this.isPair = isPair;
     }
 }
