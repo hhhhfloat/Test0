6949e0aa6977eb95d1a5a03427905cc1ff82e426
hhhhfloat <hhhhulewen@163.com>
2026-05-12 14:18:12 +0800

    初步更新存档

--- src/main/java/dao/GameSaveDao.java
+++ src/main/java/dao/GameSaveDao.java
@@ -9,8 +9,11 @@ public interface GameSaveDao {
     // 初始化用的设置用户名
     void setCurrentUser(String userName);
 
+
     // 保存地图
     void saveMap(MapSaveData mapData)throws IOException;
     MapSaveData loadMaps() throws IOException;
+    // 保存Config
+    Properties loadConfig();
     void saveConfig(Properties config);
 }
