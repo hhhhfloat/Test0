914ea0136a2737850ba07830386cce6967e8222c
VagrantX <3519344040@qq.com>
2026-05-20 22:20:03 +0800

    添加了关卡初步

--- src/main/java/view/boxes/LevelBox.java
+++ src/main/java/view/boxes/LevelBox.java
@@ -5,14 +5,41 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 public class LevelBox extends VBox{
+    private final GameCtrl gameCtrl;
+    private final Button level1 = new Button("Level 1"),  level2 = new Button("Level 2 - Locked"),  level3 = new Button("Level 3 - Locked"), level4 = new Button("Level 4 - Locked"),  level5 = new Button("Level 5 - Locked");
     public LevelBox(GameCtrl gameCtrl){
         super(40);
-        Button level1 = new Button("Level 1"),  level2 = new Button("Level 2"),  level3 = new Button("Level 3"), level4 = new Button("Level 4"),  level5 = new Button("Level 5");
+        this.gameCtrl =gameCtrl;
+
         level1.setOnAction(event -> gameCtrl.handleLevel1());
-        level2.setOnAction(event -> gameCtrl.handleLevel2());
+
         level3.setOnAction(event -> gameCtrl.handleLevel3());
         level4.setOnAction(event -> gameCtrl.handleLevel4());
         level5.setOnAction(event -> gameCtrl.handleLevel5());
         getChildren().addAll(level1, level2, level3, level4, level5);
+    }
+
+    public void Unlock(Button button) {
+
+    }
+
+    public void Unlock2(){
+        level2.setText("Level2 - Unlocked!");
+        level2.setOnAction(event -> gameCtrl.handleLevel2());
+    }
+
+    public void Unlock3(){
+        level3.setText("Level3 - Unlocked!");
+        level3.setOnAction(event -> gameCtrl.handleLevel3());
+    }
+
+    public void Unlock4(){
+        level4.setText("Level4 - Unlocked!");
+        level4.setOnAction(event -> gameCtrl.handleLevel4());
+    }
+
+    public void Unlock5(){
+        level5.setText("Level5 - Unlocked!");
+        level5.setOnAction(event -> gameCtrl.handleLevel5());
     }
 }
