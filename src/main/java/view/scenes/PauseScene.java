bea606db97eb14af683ef3cfbecc45f6bee04a5d
VagrantX <3519344040@qq.com>
2026-05-22 19:46:17 +0800

    添加了关卡初步

--- src/main/java/view/scenes/PauseScene.java
+++ src/main/java/view/scenes/PauseScene.java
@@ -9,6 +9,6 @@ import java.nio.file.Paths;
 public class PauseScene extends Scene{
     public PauseScene(GameCtrl gameCtrl) {
         super(new BorderPane(new PauseBox(gameCtrl)), 800, 800);
-        getStylesheets().add(Paths.get("src", "main", "resources", "css", "SceneStyle", "pauseSceneStyle.css").toString());
+        getStylesheets().add(Paths.get("src", "main", "resources", "css", "SceneStyle", "pauseSceneStyle.css").toUri().toString());
     }
 }
