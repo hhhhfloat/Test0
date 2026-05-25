6cfc42f85b7659e2a184c7d95c6358113c04ee83
hhhhfloat <hhhhulewen@163.com>
2026-05-25 21:03:12 +0800

    对对子完成

--- src/main/java/view/MouseGlowEffect.java
+++ src/main/java/view/MouseGlowEffect.java
@@ -15,6 +15,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.util.Duration;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -33,7 +34,8 @@ public class MouseGlowEffect {
     private final List<Timeline> activeRipples = new ArrayList<>();
     private final List<Animation> activeMeteors = new ArrayList<>();
     private static final Random random = new Random();
-    private double lastClickX = 0, lastClickY = 0;
+    private ArrayDeque<Double> lastClickX = new ArrayDeque<>();
+    private ArrayDeque<Double> lastClickY = new ArrayDeque<>();
 
     private MouseGlowEffect(Scene scene, Pane rootPane) {
         // 覆盖层（透明，不干扰交互）
@@ -86,10 +88,12 @@ public class MouseGlowEffect {
             double x = event.getX();
             double y = event.getY();
             addRippleEffect(x, y);
-            lastClickX = x;
-            lastClickY = y;
+            lastClickX.addLast(x);
+            lastClickY.addLast(y);
             PauseTransition pause = new PauseTransition(Duration.millis(random.nextInt(1000,5000)));
-            pause.setOnFinished(e -> createMeteor(lastClickX,lastClickY));
+            pause.setOnFinished(e -> {
+                createMeteor(lastClickX.pop(), lastClickY.pop());
+            });
             pause.play();
         });
 
@@ -214,7 +218,7 @@ public class MouseGlowEffect {
         double meteorBaseDurationMs = 600;
         Duration moveDuration = Duration.millis(meteorBaseDurationMs);
         Duration fadeInDuration = Duration.millis(meteorBaseDurationMs * 0.4);
-        Duration fadeOutDelay = Duration.millis(meteorBaseDurationMs * 0.5);
+        Duration fadeOutDelay = Duration.millis(meteorBaseDurationMs * 0.7);
         Duration fadeOutDuration = Duration.millis(meteorBaseDurationMs * 0.3);
 
         TranslateTransition tt = new TranslateTransition(moveDuration, meteor);
