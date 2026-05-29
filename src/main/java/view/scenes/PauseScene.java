628a816e2048f59232fe226110456e5b4a4598c3
hhhhfloat <hhhhulewen@163.com>
2026-05-30 00:37:37 +0800

    pauseScene Now can display meteor

--- src/main/java/view/scenes/PauseScene.java
+++ src/main/java/view/scenes/PauseScene.java
@@ -10,8 +10,8 @@ import java.nio.file.Paths;
 
 public class PauseScene extends Scene{
     public PauseScene(GameCtrl gameCtrl) {
-        super(new BorderPane(new PauseBox(gameCtrl)), 800, 800);
+        super(new StackPane(new PauseBox(gameCtrl)), 800, 800);
         getStylesheets().add(Paths.get("src", "main", "resources", "css", "SceneStyle", "pauseSceneStyle.css").toUri().toString());
-        MouseGlowEffect.attach(this, (BorderPane) getRoot());
+        MouseGlowEffect.attach(this, (StackPane) getRoot());
     }
 }
